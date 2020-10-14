bee80e956133dc0c1a7abab27c9ede740d4d4cee
Wassapol Pungjap <pptvstudio@gmail.com>
2020-10-14 11:54:15 +0700

    add PanelCreator

---
+++ Cyber Crime Law Experience Game/Assets/Scripts/Core/PanelCreator/PanelCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCreator : MonoBehaviour
+{
+    GameObject[] panels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panels = Resources.LoadAll<GameObject>("Prefabs/Panel");
+    }
+
+    public void InstantiatePanel(int index){
+        GameObject.Instantiate(panels[index]);
+    }
+}
---
+++ Cyber Crime Law Experience Game/Assets/Scripts/Core/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != null && instance != this){
+            Destroy(this.gameObject);
+            return;
+        }
+        if(instance == null){
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName){
+        AsyncOperation opt = UnitySceneManager.LoadSceneAsync(sceneName);
+    }
+}
