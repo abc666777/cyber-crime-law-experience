ff39eadd4ab59530c7331f388ec22647533d51e5
Wassapol Pungjap <pptvstudio@gmail.com>
2020-10-14 13:15:31 +0700

    add animations for panel

---
+++ Cyber Crime Law Experience Game/Assets/Scripts/Core/PanelClass/Panel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    public void Close(){
+        Destroy(gameObject);
+    }
+
+    public void BackToMainMenu(){
+        SceneManager.instance.LoadScene("Start");
+    }
+}
